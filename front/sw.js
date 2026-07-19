7a9a8b86b4998bb2ad36f01b9f742d8f85e55e93
Andrew Nour <andrewnour9@gmail.com>
2026-07-19 14:09:13 +0300

    Added self-destructing sw.js to automatically unregister obsolete service workers on client browsers

---
+++ front/sw.js
@@ -0,0 +1,16 @@
+// Self-destructing service worker to clear obsolete caches and unregister itself
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  self.registration.unregister()
+    .then(() => self.clients.matchAll())
+    .then((clients) => {
+      clients.forEach(client => {
+        if (client.url) {
+          client.navigate(client.url);
+        }
+      });
+    });
+});
