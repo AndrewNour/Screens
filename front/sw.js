506a05f5e83250b56782524cc13261d2b11c1842
Andrew Nour <andrewnour9@gmail.com>
2026-07-24 23:37:53 +0300

    Removed auto-reload triggers from service worker cleanup scripts to fix the tab switching refresh issue

--- front/sw.js
+++ front/sw.js
@@ -4,13 +4,5 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (e) => {
-  self.registration.unregister()
-    .then(() => self.clients.matchAll())
-    .then((clients) => {
-      clients.forEach(client => {
-        if (client.url) {
-          client.navigate(client.url);
-        }
-      });
-    });
+  self.registration.unregister();
 });
